5990dd9a3b007384d8afb5ce9d829b41880a98fc
Gabriel Santos <jgabriel.professional@gmail.com>
2023-06-21 20:48:59 -0300

    Refactoring

--- server/api/app.js
+++ server/api/app.js
@@ -11,8 +11,8 @@ app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use("/groupRouter", groupRouter);
-app.use("/paymentRouter", paymentRouter);
-app.use("/userRouter", userRouter);
+app.use("/group", groupRouter);
+app.use("/payment", paymentRouter);
+app.use("/user", userRouter);
 
 export default app;
