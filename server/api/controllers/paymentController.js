e5cd398829096b24930a8902eb47168c5a931c86
Gabriel Santos <jgabriel.professional@gmail.com>
2023-06-23 08:49:47 -0300

    Refactoring

--- server/api/controllers/paymentController.js
+++ server/api/controllers/paymentController.js
@@ -2,38 +2,43 @@ import Payment from "../../models/payment.js";
 
 import { getDateTime } from "../../helpers/getDateTime.js";
 
-export const processInitialPayment = async (request, response) => {
+export const processPayment = async (request, response) => {
     const { userId, amount } = request.body;
 
     try {
-        const existingPayment = await Payment.findOne({ user_id: userId });
-        if (existingPayment) {
-            return response.status(403).json({ message: "Payment record already exists for this user" });
-        }
-
         const currentDateAPI = await getDateTime();
         const currentDate = new Date(currentDateAPI);
 
         const dueDateAPI = currentDateAPI.add(31, "days").format("YYYY-MM-DD");
         const dueDate = new Date(dueDateAPI);
 
-        const payment = new Payment({
-            user_id: userId,
-            amount: amount,
-            due_date: dueDate,
-            status: "PAID",
-            payment_history: [{ date: currentDate, amount: amount }],
-        });
+        let payment = await Payment.findOne({ user_id: userId });
+
+        if (!payment) {
+            payment = new Payment({
+                user_id: userId,
+                amount: amount,
+                due_date: dueDate,
+                status: "PAID",
+                payment_history: [
+                    { date: currentDate, amount: amount }
+                ],
+            });
+
+        } else {
+            payment.amount = amount;
+            payment.due_date = dueDate;
+            payment.status = "PAID";
+            payment.payment_history.push(
+                { date: currentDate, amount: amount }
+            );
+        }
 
         await payment.save();
 
-        response.status(200).json({ message: "Initial payment created successfully" });
+        response.status(200).json({ message: "Payment processed successfully" });
 
     } catch (error) {
-        response.status(500).json({ message: "Error creating initial payment" });
+        response.status(500).json({ message: "Error processing payment" });
     }
-};
-
-export const registerPayment = async (request, response) => {
-
 };
