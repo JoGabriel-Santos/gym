1da86ea1a13545a14a7b6dd64e582c9953d29c37
Gabriel Santos <jgabriel.professional@gmail.com>
2023-06-25 16:27:04 -0300

    Implementing Authentication

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,22 +1,50 @@
 import React, { useState } from "react";
-import { TextInput, StyleSheet } from "react-native";
+import { TextInput, TouchableOpacity, View, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const Input = ({ ...placeHolder }) => {
     const [focused, setFocused] = useState(false);
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const isPassword = placeHolder.placeholder === "Senha";
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
 
     return (
-        <TextInput
-            style={[styles.textInput, focused && styles.focused]}
-            onBlur={() => setFocused(false)}
-            onFocus={() => setFocused(true)}
-            placeholderTextColor={"#333"}
+        <View style={styles.container}>
+            <TextInput
+                style={[styles.textInput, focused && styles.focused]}
+                onBlur={() => setFocused(false)}
+                onFocus={() => setFocused(true)}
+                placeholderTextColor={"#333"}
+                secureTextEntry={!passwordVisible && isPassword}
 
-            {...placeHolder}
-        />
+                {...placeHolder}
+            />
+
+            {
+                isPassword && (
+                    <TouchableOpacity
+                        style={styles.toggleButton}
+                        onPress={togglePasswordVisibility}
+                    >
+                        <Ionicons
+                            name={passwordVisible ? 'eye-off' : 'eye'}
+                            size={20}
+                            color="#888"
+                        />
+                    </TouchableOpacity>
+                )
+            }
+        </View>
     )
 };
 
 const styles = StyleSheet.create({
+    container: {
+        position: "relative",
+    },
     textInput: {
         backgroundColor: "#fff",
         borderColor: "#888",
@@ -29,8 +57,14 @@ const styles = StyleSheet.create({
     },
     focused: {
         borderColor: "#f9c22a",
-        borderWidth: 2,
-    }
+        borderWidth: 1,
+    },
+    toggleButton: {
+        position: "absolute",
+        right: 15,
+        top: "50%",
+        transform: [{ translateY: -12 }],
+    },
 });
 
 export default Input;
